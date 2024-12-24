7dd9aee2c8d62bdbd0f53fc30dda44193ff84a08
AndrewFagre <AndrewFagre1@gmail.com>
2024-12-24 14:00:49 -0600

    initial commit for project

---
+++ FMIT/src/com/aaf/fmit/main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.aaf.fmit.main;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
